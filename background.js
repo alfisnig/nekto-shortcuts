c9e0f2157c195c224c4f4acd6319a07185c444c5
alfisnig <valerasafin2005@gmail.com>
2022-08-12 14:42:56 +0300

    sedMessage error fixed

--- background.js
+++ background.js
@@ -1,6 +1,8 @@
 chrome.commands.onCommand.addListener((command) => {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         let current_tab = tabs[0];
-        chrome.tabs.sendMessage(current_tab.id, { "command": command });
+        if (current_tab !== undefined && current_tab.url !== undefined && current_tab.url.match('nekto.me/audiochat')) {
+            chrome.tabs.sendMessage(current_tab.id, { "command": command });
+        }
     })
 });
